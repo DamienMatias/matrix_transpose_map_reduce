7202916bbe59ef1c81966dadcdbcb7cbf6b829ca
Damien Matias <damien.matias14@gmail.com>
2017-10-05 23:07:27 +0200

    Code commented

--- map_reduce.py
+++ map_reduce.py
@@ -2,6 +2,8 @@ def map(key, value):
     intermediate = []
     for columnIndex in range(0, len(value)):
         intermediate.append({columnIndex: {key: value[columnIndex]}})
+        # The goal is to put the columnIndex as the new key because it will become the new row index
+        # We put the key in the dictionnary to help the shuffle&sort to sort
     print(intermediate)
     return intermediate
 
@@ -10,13 +12,14 @@ map(0, [1, 2, 3, 4])
 map(1, [5, 6, 7, 8])
 map(2, [9, 0, 1, 2])
 
-
+# We suppose that the shuffle and sort put all the infos in the good order
 def reduce(new_key, new_value):
     transposevalues = []
     for columnIndex in new_value:
         transposevalues.append(new_value[columnIndex])
     print({new_key: transposevalues})
     return {new_key: transposevalues}
+    # We return a list with all the values and their key
 
 
 reduce(0, {0: 1, 1: 5, 2: 9})
